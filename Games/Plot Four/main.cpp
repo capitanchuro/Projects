e207d6dbfab4cfb11eab6dfa62140e47bc20bfa2
Overtime <57847978+capitanchuro@users.noreply.github.com>
2022-07-26 21:50:21 -0700

    Update main.cpp

--- Games/Plot Four/main.cpp
+++ Games/Plot Four/main.cpp
@@ -6,7 +6,12 @@ int main(int argc, char* argv[]) {
 		SDL_WINDOWPOS_CENTERED, 
 		SDL_WINDOWPOS_CENTERED, 
 		500, 500, 
-		SDL_WINDOW_OPENGL);
+		SDL_WINDOW_SHOWN);
+
+	SDL_Renderer* render = SDL_CreateRenderer(window, -1, 0);
+	SDL_SetRenderDrawColor(render, 0, 0, 0, 255);
+	SDL_RenderClear(render);
+	SDL_RenderPresent(render);
 	SDL_Delay(16000);
 	return 0;
 }
